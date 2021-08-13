18af5d6f34b755674eaef18aac85122bb3e800fd
SHINILSUB <deathsizes@hotmail.com>
2021-08-13 18:50:54 +0900

    maker

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Header from '../header/header';
+import Footer from '../footer/footer';
+import styles from './maker.module.css'
+import { useHistory } from 'react-router-dom';
 
-const Maker = (props) => (
-    <h1>Maker</h1>
+const Maker = ({authService}) => {
+    const history = useHistory();
+    const onLogout = () => {
+        authService.onLogout();
+    };
+
+    useEffect(()=> {
+        authService.onAuthChange(user => {
+            if(!user) {
+                history.push('/')
+
+            }
+        })
+    })
+    return(
+        <section className={styles.maker}>
+            <Header onLogout={onLogout} />
+            <Footer/>
+
+        </section>
+    )
+}
+    
             
-    );
+    ;
 
 export default Maker;
--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -1,13 +1,26 @@
 import firebase from 'firebase';
 import firebaseApp from './firebase';
 
+//사용법은 firebase manual 참조
 class AuthService {
     // 서비스별로 변경됨
     login(providerName){
         const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
         return firebaseApp.auth().signInWithPopup(authProvider);
     }
+
+    logout(){
+        firebase.auth().signOut();
+    }
+
+    onAuthChange(onUserChanged) {
+        firebase.auth().onAuthStateChanged(user => {
+            onUserChanged(user);
+        })
+
+    }
     
 }
+
 
 export default AuthService
